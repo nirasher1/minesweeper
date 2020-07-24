0eca75e925586399d84126309adfe76787d3cf4c
nir <a>
2020-07-24 17:48:27 +0300

    ControlPanel and Watch modifiers fix

--- src/js/gamePage/controlPanel/ControlPanel.js
+++ src/js/gamePage/controlPanel/ControlPanel.js
@@ -1,45 +1,54 @@
 import Watch from "./watch.js";
 
-const setupElements = controlPanel => {
-    const element = controlPanel._element;
-    element.classList.add("control-panel");
 
-    controlPanel._watchElement.appendChild(document.createTextNode("time: "));
-    controlPanel._watchElement.appendChild(controlPanel.watch.render());
-
-    controlPanel._newGameButton.innerText = "START A NEW GAME";
-    controlPanel._newGameButton.addEventListener("click", controlPanel.startANewGame);
-
-    element.appendChild(controlPanel._watchElement);
-    element.appendChild(controlPanel._minesCounterElement);
-    element.appendChild(controlPanel._newGameButton);
-};
+const _setupElements = Symbol("setupElements");
+const _element = Symbol("element");
+const _watchElement = Symbol("watchElement");
+const _minesCounterElement = Symbol("minesCounterElement");
+const _newGameButtonElement = Symbol("element");
+const _minesToMarkCount = Symbol("minesToMarkCount");
+const _startANewGame = Symbol("startANewGame");
 
 export default class ControlPanel {
     constructor(minesToMarkCount, movesCount, startANewGame) {
-        this.minesToMarkCount = minesToMarkCount;
-        this.startANewGame = startANewGame;
         this.watch = new Watch();
-        this._element = document.createElement("div");
-        this._watchElement = document.createElement("span");
-        this._minesCounterElement = document.createElement("span");
-        this._newGameButton = document.createElement("button");
-        setupElements(this);
+        this[_minesToMarkCount] = minesToMarkCount;
+        this[_startANewGame] = startANewGame;
+        this[_element] = document.createElement("div");
+        this[_watchElement] = document.createElement("span");
+        this[_minesCounterElement] = document.createElement("span");
+        this[_newGameButtonElement] = document.createElement("button");
+        this[_setupElements]();
+    }
+
+    [_setupElements]() {
+        const element = this[_element];
+        element.classList.add("control-panel");
+
+        this[_watchElement].appendChild(document.createTextNode("time: "));
+        this[_watchElement].appendChild(this.watch.render());
+
+        this[_newGameButtonElement].innerText = "START A NEW GAME";
+        this[_newGameButtonElement].addEventListener("click", this[_startANewGame]);
+
+        element.appendChild(this[_watchElement]);
+        element.appendChild(this[_minesCounterElement]);
+        element.appendChild(this[_newGameButtonElement]);
     }
 
     decreaseMinesToMarkCount() {
-        this.minesToMarkCount--;
+        this[_minesToMarkCount]--;
         this.render();
     }
 
     increaseMinesToMarkCount() {
-        this.minesToMarkCount++;
+        this[_minesToMarkCount]++;
         this.render();
     }
 
     render() {
-        this._minesCounterElement.innerText = `mines: ${this.minesToMarkCount}`;
+        this[_minesCounterElement].innerText = `mines: ${this[_minesToMarkCount]}`;
 
-        return this._element;
+        return this[_element];
     }
 }
