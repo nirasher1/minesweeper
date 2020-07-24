d59a37bc4a824bd1daaa6d3b167fda4592a66e73
nir <a>
2020-07-24 17:08:26 +0300

    Fixed configurationPage folder modifires

--- src/js/configurationPage/configurationPage.js
+++ src/js/configurationPage/configurationPage.js
@@ -1,21 +1,13 @@
 import GameOption from "./gameOption.js"
 import CustomGameOption from "./customGameOption.js"
 
-
-const onOptionClick = (configurationPage, e, gameOption) => {
-    const currentSelectedOption = configurationPage.selectedOption;
-
-    if (currentSelectedOption === null) {
-        configurationPage._startButtonElement.addEventListener("click",
-            () => configurationPage.onStartGameClick(configurationPage))
-    }
-    if (currentSelectedOption !== null && currentSelectedOption !== gameOption) {
-        currentSelectedOption.unmarkAsSelectedOption();
-    }
-
-    configurationPage.selectedOption = gameOption;
-    configurationPage.selectedOption.markAsSelectedOption();
-};
+const _element = Symbol("element");
+const _startButtonElement = Symbol("startButtonElement");
+const _onOptionClick = Symbol("onOptionClick");
+const _builtinOptionsMetadata = Symbol("builtinOptions");
+const _customOptionMetadata = Symbol("customOption");
+const _onStartGameClick = Symbol("onStartGameClick");
+const _gameOptions = Symbol("gameOptions");
 
 export default class ConfigurationPage {
     constructor({
@@ -27,8 +19,9 @@ export default class ConfigurationPage {
                     minCustomMinesCount,
                     customOptionClassList,
                     onStartGameClick}) {
-        this.builtinOptions = builtinOptions;
-        this.customOption = {
+        this.selectedGameOption = null;
+        this[_builtinOptionsMetadata] = builtinOptions;
+        this[_customOptionMetadata] = {
             minCustomRowsCount,
             maxCustomRowsCount,
             minCustomColumnsCount,
@@ -36,15 +29,29 @@ export default class ConfigurationPage {
             minCustomMinesCount,
             customOptionClassList
         };
-        this.onStartGameClick = onStartGameClick;
-        this.gameOptions = [];
-        this.selectedOption = null;
-        this._element = document.createElement("div");
-        this._startButtonElement = document.createElement("button");
+        this[_onStartGameClick] = onStartGameClick;
+        this[_gameOptions] = [];
+        this[_element] = document.createElement("div");
+        this[_startButtonElement] = document.createElement("button");
     }
 
+    [_onOptionClick] (e, gameOption) {
+        const currentSelectedOption = this.selectedGameOption;
+
+        if (currentSelectedOption === null) {
+            this[_startButtonElement].addEventListener("click",
+                () => this[_onStartGameClick](this))
+        }
+        if (currentSelectedOption !== null && currentSelectedOption !== gameOption) {
+            currentSelectedOption.unmarkAsSelectedOption();
+        }
+
+        this.selectedGameOption = gameOption;
+        this.selectedGameOption.markAsSelectedOption();
+    };
+
     render() {
-        const element = this._element;
+        const element = this[_element];
         element.id = "configuration-page";
 
         const title  = document.createElement("h2");
@@ -54,21 +61,21 @@ export default class ConfigurationPage {
         gameOptionsDiv.id = "game-options";
 
 
-        this.builtinOptions.forEach(option => {
-            const gameOption = new GameOption(option, (e, gameOption) => onOptionClick(this, e, gameOption));
-            this.gameOptions.push(gameOption);
+        this[_builtinOptionsMetadata].forEach(option => {
+            const gameOption = new GameOption(option, (e, gameOption) => this[_onOptionClick].call(this, e, gameOption));
+            this[_gameOptions].push(gameOption);
             gameOptionsDiv.appendChild(gameOption.render());
         });
-        const customGameOption = new CustomGameOption(this.customOption,
-            (e, gameOption) => onOptionClick(this, e, gameOption));
+        const customGameOption = new CustomGameOption(this[_customOptionMetadata],
+            (e, gameOption) => this[_onOptionClick].call(this, e, gameOption));
         gameOptionsDiv.appendChild(customGameOption.render());
 
-        this._startButtonElement.innerText = "START GAME";
+        this[_startButtonElement].innerText = "START GAME";
 
         element.appendChild(title);
         element.appendChild(gameOptionsDiv);
-        element.appendChild(this._startButtonElement);
+        element.appendChild(this[_startButtonElement]);
 
-        document.body.appendChild(this._element);
+        document.body.appendChild(this[_element]);
     }
 }
